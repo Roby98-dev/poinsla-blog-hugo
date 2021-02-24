6696cac3b35bdfc1895d15305f4942601b02131b
I KADEK ROBY ADI PUTRA <73769308+Roby98-dev@users.noreply.github.com>
2021-02-24 15:03:47 +0800

    update Feb 24 2021

--- public/js/poinsla-site.js
+++ public/js/poinsla-site.js
@@ -1,3 +1,34 @@
+// Email.js
+(function() {
+  emailjs.init("user_O4BtmcabCOL7eiEb6EIzj");
+})();
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = Math.random() * 100000 | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm('service_13kzxz2', 'contact_form', '#contact-form')
+          .then(function() {
+              alert('Your message was sent successfully');
+          }, function(error) {
+              console.log('FAILED...', error);
+          });
+  });
+}
+
+
+// Typed
+var typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  typeSpeed: 20,
+  backSpeed: 20,
+  startDelay: 1000,
+  loop: true,
+  loopCount: Infinity
+});
+
 // AOS ANIMATION
 AOS.init({
   disable: 'mobile',
